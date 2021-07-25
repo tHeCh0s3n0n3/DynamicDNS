aac5a6f115b57318694e9cfbc708f31cb3adff34
Tarek Fadel <dev@tarekfadel.com>
2021-07-25 23:08:48 +0400

    Work complete

---
+++ IP Update API/ip.php
@@ -0,0 +1,116 @@
+<?php
+
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+if(isset($_GET['action'])) {
+    require_once("AuthKey.inc.php");
+    global $AuthKey;
+
+    $token = GetAuthToken();
+    if (null == $token || $token !== $AuthKey) {
+        http_response_code(401);
+        die();
+    }
+
+    switch ($_GET['action']) {
+        case "update":
+            ProcessUpdate();
+            break;
+        case "getAll":
+            ProcessGetAll();
+            break;
+        default:
+            die();
+    }
+}
+
+function ProcessUpdate() {
+    require_once("SQLiteConnection.inc.php");
+    $pdo = (new SQLiteConnection())->connect();
+    if (null == $pdo) {
+        echo 'Whoops, could not connect to the SQLite database!';        
+        http_response_code(500);
+        die();
+    }
+    
+    $subdomain = $_POST['subdomain'];
+    $ip = $_POST['ip'];
+
+    $sth = $pdo->prepare("SELECT * FROM Subdomains WHERE subdomain = :subdomain");
+    $sth->bindParam(":subdomain", $subdomain);
+    $sth->execute();
+    $result = $sth->fetch(PDO::FETCH_ASSOC);
+
+    $query = "";
+    if (false !== $result) {        
+        if($result['ip'] == $ip) {
+            // No updated needed.
+            http_response_code(304);
+            exit();
+        }
+        $query = "UPDATE Subdomains SET ip = :ip WHERE subdomain = :subdomain";
+    } else {
+        $query = "INSERT INTO Subdomains (subdomain, ip) VALUES (:subdomain, :ip)";
+    }
+
+    $sth2 = $pdo->prepare($query);
+    $sth2->bindParam(":ip", $ip);
+    $sth2->bindParam(":subdomain", $subdomain);
+    $sth2->execute();
+    echo "Done";
+    http_response_code(201);
+    exit();
+}
+
+function ProcessGetAll() {
+    require_once("SQLiteConnection.inc.php");
+    $pdo = (new SQLiteConnection())->connect();
+    if (null == $pdo) {
+        echo 'Whoops, could not connect to the SQLite database!';        
+        http_response_code(500);
+        die();
+    }
+
+    $sth = $pdo->prepare("SELECT * FROM Subdomains");
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($result);
+    exit();
+}
+
+/** 
+ * Get header Authorization
+ * */
+function getAuthorizationHeader(){
+    $headers = null;
+    if (isset($_SERVER['Authorization'])) {
+        $headers = trim($_SERVER["Authorization"]);
+    }
+    else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+        $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+    } elseif (function_exists('apache_request_headers')) {
+        $requestHeaders = apache_request_headers();
+        // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+        $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+        //print_r($requestHeaders);
+        if (isset($requestHeaders['Authorization'])) {
+            $headers = trim($requestHeaders['Authorization']);
+        }
+    }
+    return $headers;
+}
+/**
+* get access token from header
+* */
+function GetAuthToken($type = "Basic") {
+    $headers = getAuthorizationHeader();
+    // HEADER: Get the access token from the header
+    if (!empty($headers)) {
+        if (preg_match('/' . $type . '\s(\S+)/', $headers, $matches)) {
+            return $matches[1];
+        }
+    }
+    return null;
+}
--- Update Zone File/src/main.py
+++ Update Zone File/src/main.py
@@ -1,8 +1,152 @@
 #!/usr/bin/env python3
 
-def main():
-    print("Hello, World!")
+from argparse import ArgumentParser
+from os import path
+from pprint import pprint as pp
+from sys import exit
+from urllib.error import HTTPError
+import base64
+import datetime
+import json
+import urllib.request
+
+debug = False
+quiet = False
+
+def main(filename: str, url: str, credentials: str, subdomains: list, dbg: bool, shush: bool):
+    global debug, quiet
+    debug = dbg
+    quiet = shush
+
+    if debug:
+        print(f"Starting application from {__file__}")
+        print("INPUTS:")
+        print(f"    Zone File: {filename}")
+        print(f"          Url: {url}")
+        print(f"  Credentials: {credentials}")
+        print(f"   Subdomains: {subdomains}")
+        print(f"        Debug: {dbg}")
+        print(f"        Quiet: {shush}")
+
+    if filename[:2] == "..":
+        zoneFilePath = path.abspath(path.dirname(path.abspath(__file__))
+                                    + "/"
+                                    + filename)
+    else:
+        zoneFilePath = path.abspath(filename)
+    if debug:
+        print(f"Fetching {zoneFilePath}")
+
+    zoneFile = GetZoneFile(zoneFilePath)
+    
+    ipToTest = GetNewIPAddress(url, credentials)
+    if debug:
+        print("IPs to test: ")
+        pp(ipToTest)
+
+    updatedZoneFile = UpdateZoneFile(zoneFile, ipToTest, subdomains)
+    if debug:
+        pp(updatedZoneFile)
+    
+    if updatedZoneFile is zoneFile:        
+        retval = 1
+        if not quiet:
+            print("Zone file unchanged, exiting.")
+    else:
+        retval = 0
+        if not quiet:
+            print("Zone file changed, writing new zone file!")
+        WriteZoneFile(zoneFilePath, updatedZoneFile)
+    exit(retval)
+
+def GetZoneFile(filename: str):
+    try:
+        with open(filename, 'r') as file:
+            fileContents = file.readlines()
+    except FileNotFoundError:
+        raise
+    return fileContents
+
+
+def WriteZoneFile(filename: str, contents: list):
+    with open(filename, 'w') as file:
+        file.writelines(contents)
+
+
+def GetNewIPAddress(url: str, credentials: str):
+
+    try:
+        headers = {"Authorization": "Basic " + base64.b64encode(credentials.encode("ascii"))
+                                                     .decode("ascii")}
+        with urllib.request.urlopen(
+            urllib.request.Request(url, headers=headers, method="GET")) as response:
+            newIPs = json.loads(response.read())
+    except HTTPError as err:
+        if err.code == 401:
+            if not quiet:
+                print("HTTP Authentication failed. Quitting")
+                exit(2)
+
+    return newIPs
+
+
+def UpdateZoneFile(zoneFile, ipsToTest, subdomains):
+    newZoneFile = []
+    zoneFileUpdated = False
+
+    foundSubdomain = False
+    if len([item for item in ipsToTest if item["subdomain"] in subdomains]) > 0:
+        foundSubdomain = True
+
+    if not foundSubdomain:
+        if debug:
+            print("Subdomain not found in list from server")
+        return zoneFile
+    
+    updatedSerial = False
+    now = datetime.datetime.now()
+    newSerial = "".join([str(now.year)
+                         , str(now.month).zfill(2)
+                         , str(now.day).zfill(2)
+                        ])
+    for line in zoneFile:
+        lineParts = line.split()
+        if line[0] == ";" or len(lineParts) == 0:
+            newZoneFile.append(line)
+            continue
+        if len(lineParts) == 1 and not updatedSerial:
+            if str(lineParts[0]).startswith(newSerial):
+                # we need to update the serial number
+                newSerial += str(int(str(lineParts[0])[-2:]) + 1).zfill(2)
+            else:
+                newSerial += "01"
+            updatedSerial = True
+            newZoneFile.append(line.replace(lineParts[0], newSerial))
+    
+        elif lineParts[0] in subdomains:
+            if debug:
+                print("Found a subdomain we want to change!")
+            newIP = next(item for item in ipsToTest if item["subdomain"] == lineParts[0])
+            newZoneFile.append(line.replace(lineParts[3], newIP["ip"]))
+            if newZoneFile[-1] != line:
+                zoneFileUpdated = True
+        else:
+            newZoneFile.append(line)
+    
+    if zoneFileUpdated:
+        return newZoneFile
+    else:
+        return zoneFile
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(description="Updates a BIND9/NSD zone file's subdomains")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verbose", action="store_true", help="Show verbose output")
+    group.add_argument("-q", "--quiet", action="store_true", help="Suppress all output")
+    parser.add_argument("-u", "--url", help="URL from which to fetch the data")
+    parser.add_argument("-c", "--credentials", help="Credentials to use in the HTTP Authorization header (username:password)")
+    parser.add_argument("-s", "--subdomains", help="Subdomains to update")
+    parser.add_argument("zonefile", help="Zone file to update")
+    args = parser.parse_args()
+    main(args.zonefile, args.url, args.credentials, str(args.subdomains).split(","), args.verbose, args.quiet)
