82638580b36e1b8d92f6e4281ed245cedcc67451
Tarek Fadel <dev@tarekfadel.com>
2022-08-28 12:09:45 +0400

    Adjust the argument descriptions and required indicators

--- Update Client/src/main.py
+++ Update Client/src/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from urllib.error import HTTPError
 from furl import furl
+from urllib.error import HTTPError
 import base64
 import json
-import urllib.request
 import logging
+import urllib.request
 
 def main(url: str, credentials: str, subdomains: list, dbg: bool, shush: bool, isCron: bool):
     level = logging.INFO
@@ -76,9 +76,9 @@ if __name__ == "__main__":
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-v", "--verbose", action="store_true", help="Show verbose output")
     group.add_argument("-q", "--quiet", action="store_true", help="Suppress all output")
-    parser.add_argument("-u", "--url", help="URL to trigger the update")
-    parser.add_argument("-c", "--credentials", help="Credentials to use in the HTTP Authorization header (username:password)")
-    parser.add_argument("-s", "--subdomains", help="Subdomains to update")
+    parser.add_argument("-u", "--url", help="URL to trigger the update", required=True)
+    parser.add_argument("-c", "--credentials", help="Credentials to use in the HTTP Authorization header (username:password)", required=True)
+    parser.add_argument("-s", "--subdomains", help="Subdomains to update separated by a comma \",\"", required=True)
     parser.add_argument("-t", "--timer-job", action="store_true", help="Indicate that this is running from a cron job/systemd timer")
     args = parser.parse_args()
     main(args.url, args.credentials, str(args.subdomains).split(","), args.verbose, args.quiet, args.timer_job)
